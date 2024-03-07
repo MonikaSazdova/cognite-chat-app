0c0ee8c4d69dbea5017d20a67cba304bf0050da4
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2024-03-07 22:26:56 +0100

    Add main header

---
+++ src/components/Header/MainHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import cognite_logo from '../../assets/cognite_logo.webp';
+
+const MainHeader = () => {
+	return (
+		<header className="bg-white h-16 w-full fixed top-0 left-0 flex items-center px-4 z-10">
+			<img src={cognite_logo} alt="Logo" className="h-10"/>
+		</header>
+	)
+}
+
+export default MainHeader;
