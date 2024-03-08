b32d3c242276bc4140ac02d23aafa82f82476d7f
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2024-03-08 22:45:27 +0100

    Add Contact Card

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,6 +1,11 @@
+import ContactCard from "./ContactCard";
 
 function ContactsList() {
-  return <div className="flex-1 w-2/3 bg-gray-300">Contacts List</div>;
+  return (
+    <div className="flex-1 w-2/3 bg-gray-300 p-3">
+      <ContactCard name="Monika Sazdova" message="This is a slightly long message, let's see how it will positioned in our card" time="09.32"/>
+    </div>
+  )
 }
 
 export default ContactsList;
