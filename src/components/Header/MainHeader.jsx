ccdea6149065b2bbdb8015c290270050828b4286
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2024-03-09 16:31:04 +0100

    Update backgrounds + add a settings icon + add user's profile picture

--- src/components/Header/MainHeader.jsx
+++ src/components/Header/MainHeader.jsx
@@ -2,8 +2,9 @@ import cognite_logo from '../../assets/cognite_logo.webp';
 
 const MainHeader = () => {
 	return (
-		<header className="bg-white h-16 w-full fixed top-0 left-0 flex items-center px-4 z-10">
-			<img src={cognite_logo} alt="Logo" className="h-10"/>
+		<header className="bg-gray-100 h-16 w-full fixed top-0 left-0 flex items-center px-4 z-10 justify-between">
+			<img src={cognite_logo} alt="Logo" className="h-10" />
+			<div className="rounded-full w-12 h-12 bg-gray-200 flex items-center justify-center">MS</div>
 		</header>
 	)
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,10 @@
+import icon_settings from '../../assets/icon_settings.png'
+
 
 function Sidebar() {
-  return <div className="w-16 bg-gray-200">Sidebar Content</div>;
+  return <div className="w-16 bg-gray-100 h-full flex flex-col justify-end items-center pb-4">
+      <img src={icon_settings} alt="Settings" className="h-7 w-7" />
+  </div>;
 }
 
 export default Sidebar;
