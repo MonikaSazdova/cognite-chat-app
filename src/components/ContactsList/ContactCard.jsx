227aeb5b2dcde38464fc2c3213de898a5091e494
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2024-03-10 14:43:59 +0100

    Add onClick functionality for contacts

--- src/components/ContactsList/ContactCard.jsx
+++ src/components/ContactsList/ContactCard.jsx
@@ -1,8 +1,8 @@
 import InitialsAvatar from "../User/InitialsAvatar";
 
-function ContactCard({ name, surname }) {
+function ContactCard({ name, surname, onClick }) {
   return (
-    <div className="p-3 bg-slate-100 flex flex-wrap items-center gap-2 mb-1 rounded-lg cursor-pointer">
+    <div className="p-3 bg-slate-100 flex flex-wrap items-center gap-2 mb-1 rounded-lg cursor-pointer" onClick={onClick}>
       <InitialsAvatar name={name} surname={surname} />
       <div className="flex-1 min-w-0">
         <div className="font-semibold truncate">{`${name} ${surname}`}</div>
--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,21 +1,39 @@
 import ContactCard from "./ContactCard";
+import { useEffect } from "react";
 import { useUser } from "../../context/UserContext";
 import { users } from "../../data";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  setSelectedContact,
+  setContacts,
+} from "../../store/actions/chatActions";
 
 function ContactsList() {
+  const dispatch = useDispatch();
   const loggedUser = useUser();
-  const userContactsIds = loggedUser.contacts;
+  const userContacts = useSelector((state) => state.chat.contacts);
+
+  useEffect(() => {
+    const userContactsIds = loggedUser.contacts;
+    const contacts = getUserContacts(userContactsIds);
+    dispatch(setContacts(contacts));
+  }, [loggedUser, dispatch]);
 
   const getUserContacts = (ids) => {
-    return users.filter((user) => ids.includes(user.userId));
-  };
+    users.filter((user) => ids.includes(user.userId));
+  }
 
-  const userContacts = getUserContacts(userContactsIds);
+  const onCardClick = (contact) => () => dispatch(setSelectedContact(contact));
 
   return (
     <div className="flex-1 w-2/3 bg-white p-3 rounded-tl-lg border border-gray-300 overflow-x-auto">
       {userContacts.map((contact, i) => (
-        <ContactCard key={i} name={contact.name} surname={contact.surname} />
+        <ContactCard
+          key={i}
+          name={contact.name}
+          surname={contact.surname}
+          onClick={onCardClick(contact)}
+        />
       ))}
     </div>
   );
