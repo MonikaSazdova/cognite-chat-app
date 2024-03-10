a848f1bf669c8f0f7a02ebb680ff836f2b6f54bf
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2024-03-10 19:53:20 +0100

    Decrease avatar size when avatar is located in a header

--- src/components/User/InitialsAvatar.jsx
+++ src/components/User/InitialsAvatar.jsx
@@ -5,12 +5,12 @@ function InitialsAvatar({ user, isHeader }) {
     return `${name[0].toUpperCase()}${surname[0].toUpperCase()}`;
   };
 
-  const avatarSize = isHeader ? "w-12 h-12" : "w-14 h-14";
+  const avatarSize = isHeader ? "w-10 h-10" : "w-14 h-14";
   const avatarBackground = { backgroundColor: getColorForUserId(user.userId) };
 
   return (
     <div
-      className={`rounded-full w-14 h-14  flex items-center justify-center ${avatarSize}`}
+      className={`rounded-full flex items-center justify-center ${avatarSize}`}
       style={avatarBackground}
     >
       {getInitials(user.name, user.surname)}
