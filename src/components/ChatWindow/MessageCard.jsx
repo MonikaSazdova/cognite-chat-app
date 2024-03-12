fb2c5c543ab1f570b915f4280adf676520e6ba39
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2024-03-12 19:54:50 +0100

    Switch messages side (receiving left, sent right)

--- src/components/ChatWindow/MessageCard.jsx
+++ src/components/ChatWindow/MessageCard.jsx
@@ -9,8 +9,8 @@ function MessageCard({ text, timestamp, userId }) {
 
   return (
     <div className="w-full bg-transparent">
-      <div className={`${isMessageFromContact ? "justify-end" : "justify-start"} flex`}>
-        <div className={`rounded-lg p-3 min-w-36 break-words ${isMessageFromContact ? "bg-blue-300" : "bg-white"}`}
+      <div className={`${!isMessageFromContact ? "justify-end" : "justify-start"} flex`}>
+        <div className={`rounded-lg p-3 min-w-36 break-words ${!isMessageFromContact ? "bg-blue-300" : "bg-white"}`}
           style={{boxShadow:"0 1px 2px 0 rgba(0, 0, 0, 0.05), 0 1px 3px 1px rgba(0, 0, 0, 0.1)", maxWidth: "80%"}}
         >
           <p className="text-left">{formatTextWithNewLines(text)}</p>
