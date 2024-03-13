7386e3983fdcbdf3f11f21e0639564c97898cdbe
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2024-03-13 13:15:36 +0100

    Update gist link

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -4,7 +4,8 @@ import useFetch from '../hooks/useFetch';
 export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
-  const { data, loading, error } = useFetch("https://gist.githubusercontent.com/bites2bytes/20b97cdb45c73a0430d48a021e36cf6e/raw/291d137a234650c06049e0353691c06133156c1b/chatData.json");
+  const link = "https://gist.githubusercontent.com/bites2bytes/20b97cdb45c73a0430d48a021e36cf6e/raw/36f5cc2e9392d34c86aa1b57bff178d4ad9dc216/chatData.json"
+  const { data, loading, error } = useFetch(link);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
