74e5161199f9a2fd4d2735d49f565c79fb02164f
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2024-03-13 23:50:49 +0100

    Decrease message text size

--- src/components/ChatWindow/MessageCard.jsx
+++ src/components/ChatWindow/MessageCard.jsx
@@ -12,7 +12,7 @@ const MessageCard = ({ text, timestamp, userId }) => {
         <div className={`rounded-lg p-3 min-w-36 break-words ${!isMessageFromContact ? "bg-blue-300" : "bg-white"}`}
           style={{boxShadow:"0 1px 2px 0 rgba(0, 0, 0, 0.05), 0 1px 3px 1px rgba(0, 0, 0, 0.1)", maxWidth: "80%"}}
         >
-          <p className="text-left">{formatTextWithNewLines(text)}</p>
+          <p className="text-left text-sm">{formatTextWithNewLines(text)}</p>
           <p className="text-xs text-right mt-2">{formattedTime}</p>
         </div>
       </div>
